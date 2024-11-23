4bd7ee011883b0466b40c98141101b8909beb455
김선경 <sk@gimseongyeong-ui-MacBookPro.local>
2024-11-23 14:19:20 +0900

    ootd와 cache 클래스 선언

--- server/ootd/ootd.py
+++ server/ootd/ootd.py
@@ -1,6 +1,15 @@
-import sys
+class OOTD:
+    def __init__(self, api_key):
+        self.api_key = api_key
+        self.cache = Cache()
+    
+    def handle(params):
+        #param 이용해서 
+        #1. cache 확인
+        #2. cache에 내용 존재 -> return
+        #3. cache에 내용 존재X -> api 호출, return
+        return
 
-if __name__ == "__main__" :
-    argument = sys.argv
-    print(argument[1])
-    print("hello world")
+class Cache:
+    def __init__(self):
+        return
