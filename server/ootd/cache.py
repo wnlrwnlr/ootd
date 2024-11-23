79a13e3242d3ceaf5919bc8d1518b1cd77d9d65d
김선경 <sk@gimseongyeong-ui-MacBookPro.local>
2024-11-23 17:31:41 +0900

    cachelayer 작성

--- server/ootd/cache.py
+++ server/ootd/cache.py
@@ -1,17 +1,12 @@
 class Cache:
     def __init__(self):
-        print("cache init")
-        return
-
+        print("@@@ cache > init")
+        self.storage = {} 
+    
     def get(self, params):
-        print("cache get"+ "params: " + str(params))
-        # params를 key로 해서
-        # key value storage에서 값을 조회
-        # 있으면 return 없으면 none return
-        return None
-
+        print("@@@ cache > get" + " params: " + str(params))
+        return self.storage.get(str(params), None)
+    
     def set(self, params, response):
-        print("cache set" + "params: " + str(params) + "response: " + str(response))
-        # params를 key로 해서
-        # key value storage에서 response를 저장
-        return
+        print("@@@ cache > set " + " params: " + str(params) + " response: " + str(response))
+        self.storage[str(params)] = response
