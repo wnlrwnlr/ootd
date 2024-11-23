7eaf16e82025c594bd469ea64bf2fafe90a547c8
김선경 <sk@gimseongyeong-ui-MacBookPro.local>
2024-11-23 15:09:07 +0900

    repository 클래스를 별개 파일로 작성

---
+++ server/ootd/repository.py
@@ -0,0 +1,11 @@
+class Repository:
+    def __init__(self, api_key):
+        print("repositroy init" + "api_key: " + str(api_key))
+        self.api_key = api_key
+
+    def get(self, params):
+        print("repository get" + "params: " + str(params))
+        # params를 이용해서
+        # 적절히 openAI api를 호출해서
+        # 추천값을 받아와서 return
+        return None
