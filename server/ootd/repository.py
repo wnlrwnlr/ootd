6c57ded06c0dd185083a0fbac11e87e66693db8a
Mulmin <ymjk2000@gmail.com>
2024-11-23 17:14:24 +0900

    이미지 모델로 의상 이미지 생성

--- server/ootd/repository.py
+++ server/ootd/repository.py
@@ -1,5 +1,6 @@
 import requests
 
+
 class Repository:
     def __init__(self, api_key):
         print("repositroy init" + "api_key: " + str(api_key))
@@ -9,11 +10,12 @@ class Repository:
         print("repository get" + "params: " + str(params))
 
         try:
-            print(self.__request_chat(params))
+            description = self.__request_chat(params)
+            image = self.__request_image(description)
+            return description, image
         except ValueError as e:
             print(e)
-
-        return None
+            return None
 
     def __request_chat(self, parms):
         # API URL 및 헤더 설정
@@ -31,7 +33,7 @@ class Repository:
             "messages": [
                 {
                     "role": "system",
-                    "content": "너는 주어진 상황에 맞는 옷을 추천해주는 비서야"
+                    "content": "너는 주어진 상황에 맞는 옷을 추천해주는 비서야. 주어진 상황에 대해 의상 추천을 간결하게 해줘. 앞뒤에 사족을 붙이지 마."
                 },
                 {
                     "role": "user",
@@ -45,6 +47,35 @@ class Repository:
 
         # 응답 반환
         if response.status_code == 200:
-            return "Response:", response.json()
+            return response.json()["choices"][0]["message"]["content"]
         else:
             raise ValueError(response.status_code, response.text)
+
+    def __request_image(self, description):
+        print(description)
+
+        # API URL 및 헤더 설정
+        url = "https://api.openai.com/v1/images/generations"
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {self.api_key}"
+        }
+
+        # 요청 데이터 설정
+        data = {
+            "model": "dall-e-3",
+            "prompt": f"의상을 추천해주는 비서로부터 {description} 라는 대답을 들었어. 어떤 의상인지 상상이 되지 않는데 의상의 생김새를 묘사해줘.",
+            "n": 1,
+            "size": "1024x1024"
+        }
+
+        # POST 요청 보내기
+        response = requests.post(url, headers=headers, json=data)
+
+        # 응답 반환
+        if response.status_code == 200:
+            return response.json()["data"][0]["url"]
+        else:
+            raise ValueError(response.status_code, response.text)
+
+
