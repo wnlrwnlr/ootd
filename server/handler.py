e7e5b06d7670aa000fbb070cbbba0c0c50efdac5
김선경 <sk@gimseongyeong-ui-MacBookPro.local>
2024-12-15 16:23:26 +0900

    캐시 히트/미스 로깅 추가

--- server/handler.py
+++ server/handler.py
@@ -2,11 +2,12 @@ from cache import Cache
 from repository import Repository
 import os
 from dotenv import load_dotenv
+import sentry_sdk
 
 
 class Handler:
     def __init__(self):
-        print("@@@ ootd > init")
+        sentry_sdk.capture_message("Handler initialized")
 
         load_dotenv()
         api_key = os.getenv('API_KEY')
@@ -15,13 +16,21 @@ class Handler:
         self.repository = Repository(api_key=api_key)
 
     def handle(self, params):
-        print("@@@ ootd > handle" + " params: " + str(params))
-        cached = self.cache.get(params=params)
+        sentry_sdk.capture_message(f"Handling request with params: {params}")
 
-        if cached:
-            return cached
-        else:
-            response = self.repository.get(params=params)
-            if response:
-                self.cache.set(params=params, response=response)
-            return response
+        try:
+            cached = self.cache.get(params=params)
+
+            if cached:
+                sentry_sdk.capture_message("Cache hit")
+                return cached
+            else:
+                sentry_sdk.capture_message("Cache miss")
+                response = self.repository.get(params=params)
+                if response:
+                    self.cache.set(params=params, response=response)
+                return response
+
+        except Exception as e:
+            sentry_sdk.capture_exception(e)
+            raise e
